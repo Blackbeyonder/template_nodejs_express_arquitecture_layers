e517c6112d639b6d713a0fb87a890ff89d7afb8a
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-04-05 22:52:53 -0600

    modificated: index.js  - change in port

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
-
+const serverConfig = require('./config/server');
+const PORT =  serverConfig.port;
 app.use('/api', userRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
